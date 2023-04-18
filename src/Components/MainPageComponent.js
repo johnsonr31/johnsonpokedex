3a89ce331c6797aa9a4376bafef83fe868da04ad
ricRobo4 <artistr3@proton.me>
2023-04-17 21:41:33 -0700

    * Added content to Main Page Component * Added Favicon and linked it in inded.html

--- src/Components/MainPageComponent.js
+++ src/Components/MainPageComponent.js
@@ -1,17 +1,34 @@
-import { Container, Row, Col } from "react-bootstrap"
+import { Container, Row, Col, Form } from "react-bootstrap";
+import { useEffect, useState } from "react";
+
 
 function MainPageComponent() {
-  return (
-    <div>
-        <Container fluid>
-            <Row>
-                <Col>
-                    <h1>Pokemon</h1>
-                </Col>
-            </Row>
-        </Container>
-    </div>
-  )
+  async function FetchMon () {
+    const promise = await fetch('https://pokeapi.co/api/v2/pokemon/quilava/');
+    const data = await promise.json();
+    
+  
+  }
+  
+    return (
+      <div>
+          <Container fluid>
+              <Row>
+                  <Col>
+                    <h1>Pokémon API</h1>
+                  </Col>
+              </Row>
+              <Row>
+                  <Col>
+                    <Form.Control type="text" placeholder="Enter Pokémon Name Here" />
+                  </Col>
+                  <Col>
+                    <div className="search-btn">Search for a Pokémon</div>
+                  </Col>
+              </Row>
+          </Container>
+      </div>
+    )
 }
 
 export { MainPageComponent }
--- src/Services/DataService.js
+++ src/Services/DataService.js
@@ -1,6 +1,6 @@
 async function FetchMon () {
     const promise = await fetch('https://pokeapi.co/api/v2/pokemon/quilava/');
     const data = await promise.json();
-
     
+
 }
