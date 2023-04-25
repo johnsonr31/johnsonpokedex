cbb4ccee85b7f022bdf241d1cf57a04eee8652ad
ricRobo4 <artistr3@proton.me>
2023-04-25 13:15:11 -0700

    2 Changes:  Hover to view * Added pokemon and searchTerm usestates in MainPageComponent.js * Added more elements in MainPageComponent.js * Added styles for elements in index.css

--- src/Components/MainPageComponent.js
+++ src/Components/MainPageComponent.js
@@ -3,29 +3,67 @@ import { useEffect, useState } from "react";
 
 
 function MainPageComponent() {
+
+  let pokemonData = '';
+  const [ pokemon, setPokemon ] = useState('');
+  const [ searchTerm, setSearchTerm ] = useState('');
+
   async function FetchMon () {
     const promise = await fetch('https://pokeapi.co/api/v2/pokemon/quilava/');
     const data = await promise.json();
-    
+    console.log(data);
   
   }
+
+  FetchMon();
   
     return (
       <div>
           <Container fluid>
               <Row>
                   <Col>
-                    <h1>Pokémon API</h1>
+                    <h1 className="page-title">Pokémon App</h1>
                   </Col>
               </Row>
-              <Row>
-                  <Col>
+              <Row className="search-row">
+                  <Col xs={4} className="search-bar">
                     <Form.Control type="text" placeholder="Enter Pokémon Name Here" />
                   </Col>
-                  <Col>
-                    <div className="search-btn">Search for a Pokémon</div>
+                  <Col xs={2} className="search-btn-col">
+                    <div className="search-btn">Search for Pokémon</div>
                   </Col>
               </Row>
+          </Container>
+          <Container>
+            <Row>
+              <Col>
+                number here
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                Name:
+              </Col>
+              <Col>
+                name here
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                Types:
+              </Col>
+              <Col>
+                types here
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                Types:
+              </Col>
+              <Col>
+                types here
+              </Col>
+            </Row>
           </Container>
       </div>
     )
