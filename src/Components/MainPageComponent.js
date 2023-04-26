abf480377cb57550722b1aae430269480778d126
ricRobo4 <artistr3@proton.me>
2023-04-26 14:57:16 -0700

    * Implemented button to get random Pokemon

--- src/Components/MainPageComponent.js
+++ src/Components/MainPageComponent.js
@@ -5,10 +5,14 @@ import { useEffect, useState } from "react";
 function MainPageComponent() {
 
   let pokemonData = '';
+  let randomNumber = 0;
+  // const blankIconUrl = '../Assets/Images/blankPokemon.png';
+
   const [ pokemon, setPokemon ] = useState('');
   // const [ types, setTypes ] = useState([]);
   const [ searchTerm, setSearchTerm ] = useState('');
   const [ monName, setMonName ] = useState('');
+  const [ pokemonTypes, setPokemonTypes ] = useState([]);
 
   const handleSearch = () => {
     setMonName(searchTerm);
@@ -23,10 +27,35 @@ function MainPageComponent() {
 
     setPokemon(data);
     console.log(data.types[0].type.name);
+    let typesArray = data.types;
+    let types = [];
+
+    // for(let i = 0; i < typesArray.length; i++)
+    // {
+    //   types = typesArray.push(data.types[i].type.name);
+    // }
+    // setPokemonTypes(types);
+    // console.log(types + 'hlol');
     // setTypes(data.types);
     // console.log(pokemon);
   }
+  
+  async function FetchRandomMon () {
+    randomNumber = Math.floor(Math.random() * 646);
 
+    const promise = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}/`);
+
+
+    const data = await promise.json();
+    console.log(data);
+    pokemonData = data;
+
+    setPokemon(data);
+    // console.log(data.types[0].type.name);
+    // let typesArray = data.types;
+    // let types = [];
+  }
+  
   // FetchMon();
   
     return (
@@ -35,7 +64,7 @@ function MainPageComponent() {
               <Row>
                   <Col className="header-image-column">
                     {/* <h1 className="page-title">Richard's Pokédex</h1> */}
-                    <img className="header-image" src={require('../Assets/pokemonPageImage.png')}/>
+                    <img className="header-image" src={require('../Assets/Images/pokemonPageImage.png')}/>
                   </Col>
               </Row>
               <Row className="search-row">
@@ -48,6 +77,14 @@ function MainPageComponent() {
                     <button 
                     onClick={FetchMon}
                     className="search-btn">Search for Pokémon</button>
+                  </Col>
+                  <Col xs={2} className="search-btn-col">
+                    <button
+                    className="random-btn"
+                    onClick={FetchRandomMon}
+                    >
+                      Get a random Pokémon
+                    </button>
                   </Col>
               </Row>
           </Container>
